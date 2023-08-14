b897f9f8b45de5afa32213368e87a6426726ae9a
Anand kumar <119012966+anandkumarpatel1@users.noreply.github.com>
2023-08-14 23:35:11 +0530

    Update Contact.jsx

--- src/component/layout/contact/Contact.jsx
+++ src/component/layout/contact/Contact.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import "./style.css"
-import { Button } from '@mui/material'
+import React from "react";
+import "./style.css";
+import { Button } from "@mui/material";
+import MetaData from "../metaData/MetaData";
 const Contact = () => {
   return (
-    <div className="contactContainer">
-    <a className="mailBtn" href="mailto:helloengg.420@gmail.com">
-      <Button>Contact: helloengg.420@gmail.com</Button>
-    </a>
-  </div>
-  )
-}
+    <>
+      <MetaData title="Loot Bazar - Contact" />
+      <div className="contactContainer">
+        <a className="mailBtn" href="mailto:helloengg.420@gmail.com">
+          <Button>Contact: helloengg.420@gmail.com</Button>
+        </a>
+      </div>
+    </>
+  );
+};
 
-export default Contact
+export default Contact;
