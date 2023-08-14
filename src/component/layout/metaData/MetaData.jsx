de60f887afcb292801a130d6b99e9a5a501af614
Anand kumar <119012966+anandkumarpatel1@users.noreply.github.com>
2023-08-14 23:24:47 +0530

    Update MetaData.jsx

--- src/component/layout/metaData/MetaData.jsx
+++ src/component/layout/metaData/MetaData.jsx
@@ -2,12 +2,22 @@ import React from "react";
 
 import Helmet from "react-helmet";
 
-const MetaData = ({ title }) => {
+const MetaData = ({ title, desc, keyword }) => {
   return (
     <Helmet>
       <title>{title}</title>
+      <meta
+        name="description"
+        content={desc}
+      />
+      <meta
+        name="keywords"
+        content={keyword}
+      />
+      <meta name="author" content="Anand kumar" />
     </Helmet>
   );
 };
 
 export default MetaData;
+
