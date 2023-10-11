4e385bedbae98e5e7ee533607311668beb19c3b1
Anand kumar <119012966+anandkumarpatel1@users.noreply.github.com>
2023-10-11 15:27:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App() {
   }, []);
   
 
-  // window.addEventListener("contextmenu", (e) => e.preventDefault());
+  window.addEventListener("contextmenu", (e) => e.preventDefault());
 
   return (
     <>
